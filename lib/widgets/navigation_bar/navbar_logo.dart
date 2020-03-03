c628f148afa311db360b947c49ae448cc675ba2f
Vikrant Singh <vikrant@geekyants.com>
2020-03-03 18:23:01 +0530

    adding logo

--- lib/widgets/navigation_bar/navbar_logo.dart
+++ lib/widgets/navigation_bar/navbar_logo.dart
@@ -3,10 +3,36 @@ import 'package:flutter/material.dart';
 class NavBarLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 80,
-      width: 150,
-      child: Image.asset('assets/images/logo.png'),
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Image.asset(
+            'assets/images/academyLogo.jpeg',
+            height: 70,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            // color: Colors.amber,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'GeekyAnts',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                Text('academy',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                    textAlign: TextAlign.left),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
