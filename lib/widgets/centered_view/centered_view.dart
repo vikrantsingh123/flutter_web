f13172a9e4bdc6f8ffe1fd11d296b10fb2a53461
Vikrant Singh <vikrant@geekyants.com>
2020-03-09 12:00:27 +0530

    centered_view added background image

--- lib/widgets/centered_view/centered_view.dart
+++ lib/widgets/centered_view/centered_view.dart
@@ -1,3 +1,4 @@
+import 'package:demo/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class CenteredView extends StatelessWidget {
@@ -5,11 +6,25 @@ class CenteredView extends StatelessWidget {
   const CenteredView({@required this.child});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 70, vertical: 60),
-      alignment: Alignment.topCenter,
-      child: ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 1200), child: child),
+    return Stack(
+      children: <Widget>[
+        Align(
+          alignment: Alignment.centerRight,
+          child: Container(
+            height: 500,
+            child: Image.asset('assets/images/background.jpg'),
+          ),
+        ),
+        Container(
+            // color: Colors.yellow,
+            padding: EdgeInsets.symmetric(horizontal: 70, vertical: 30),
+            // alignment: Alignment.center,
+            // width: double.infinity,
+            child: child
+            // child: ConstrainedBox(
+            //     constraints: BoxConstraints(maxWidth: 1200), child: child),
+            )
+      ],
     );
   }
 }
