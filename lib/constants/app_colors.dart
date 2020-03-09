87d229b7c468b90f6d5f3861b454d5dd8186c403
Vikrant Singh <vikrant@geekyants.com>
2020-03-09 11:51:28 +0530

    color constant added

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColor = Color.fromARGB(255, 31, 229, 146);
+// const Color primaryColor = Color.fromARGB(255, 31, 229, 146);
+const Color primaryColor = Color(0xfff44336);
+const Color cardColor = Color(0xff174051);
